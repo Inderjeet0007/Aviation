7452d857648e101c28d1b3cdf8925f3d56d6db86
inderjeet0007 <inderjeet.saluja97@gmail.com>
2018-04-13 19:44:46 +0530

    new login

--- login/index.php
+++ login/index.php
@@ -7,7 +7,7 @@ require_once('settings.php');
 $login_url = 'https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.email&' . urlencode('https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/plus.me') . '&redirect_uri=' . urlencode('http://localhost/Aviation/homepage_as.php') . '&response_type=code&client_id=838028339139-k7lk53rrui64rf48f66tfi5stjbn5gat.apps.googleusercontent.com' . '&approval_prompt=force'.'&access_type=online';
 ?>
 <head>
-	<title>LOGIN TO Aviation.com</title>
+	<title>Login: Aviation.com</title>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 	<!--===============================================================================================-->
@@ -38,80 +38,28 @@ $login_url = 'https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.goo
 		<div class="container-login100" style="background-image: url('images/bg-01.jpg');">
 			<div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
 				<form class="login100-form validate-form">
-					<span class="login100-form-title p-b-49">
-						Login
-					</span>
-
-					<div class="wrap-input100 validate-input m-b-23" data-validate = "Username is reauired">
-						<span class="label-input100">Username</span>
-						<input class="input100" type="text" name="username" placeholder="Type your username">
-						<span class="focus-input100" data-symbol="&#xf206;"></span>
-					</div>
-
-					<div class="wrap-input100 validate-input" data-validate="Password is required">
-						<span class="label-input100">Password</span>
-						<input class="input100" type="password" name="pass" placeholder="Type your password">
-						<span class="focus-input100" data-symbol="&#xf190;"></span>
-					</div>
-
-					<div class="text-right p-t-8 p-b-31">
-						<a href="#">
-							Forgot password?
-						</a>
-					</div>
-
 					<div class="container-login100-form-btn">
 						<div class="wrap-login100-form-btn">
 							<div class="login100-form-bgbtn"></div>
 							<button class="login100-form-btn">
-								Login
+								LOGIN Below
 							</button>
 						</div>
 					</div>
-
-					<div class="txt1 text-center p-t-54 p-b-20">
-						<span>
-							Or Sign Up Using
-						</span>
-					</div>
-
-					<div class="flex-c-m">
-						<a href="#" class="login100-social-item bg1">
-							<i class="fa fa-facebook"></i>
-						</a>
-
-						<a href="#" class="login100-social-item bg2">
-							<i class="fa fa-twitter"></i>
-						</a>
-
-						<a href="<?= $login_url ?>" class="login100-social-item bg3">
+						<br><div align="center"><a href="<?= $login_url ?>"><h2>USER LOGIN</h2>
 							<i class="fa fa-google"></i>
-						</a>
+						</a><br>
+						<br><h3>OR</h3>
+						<br><a href="http://localhost/Aviation/admin/admin.php"><h2>Admin Login</h2></a></div>
 					</div>
-
-					<div class="flex-col-c p-t-155">
-						<span class="txt1 p-b-17">
-							Or Sign Up Using
-						</span>
-
-						<a href="#" class="txt2">
-							Sign Up
-						</a>
 					</div>
 				</form>
 			</div>
 		</div>
 	</div>
-
 	<div><p>&copy Created By Inderjeet Saluja & Nidhi Mishra</a></div>
 	<div class="fb-login-button" data-max-rows="1" data-size="large" data-button-type="continue_with" data-show-faces="true" data-auto-logout-link="true" data-use-continue-as="true"></div>
 <div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.12';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
+
 </body>
 </html>
